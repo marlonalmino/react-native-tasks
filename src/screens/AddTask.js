1059af417179d8606784c8fab56bcd483da7103a
Marlon Almino <alminomarlon@gmail.com>
2023-03-31 20:00:42 -0300

    Salvar Tarefa

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -21,6 +21,16 @@ export default class AddTask extends Component {
         ...initialState
     }
 
+    save = () => {
+        const newTask = {
+            desc: this.state.desc,
+            date: this.state.date
+        }
+
+        this.props.onSave && this.props.onSave(newTask)
+        this.setState({ ...initialState })
+    }
+
     getDatePicker = () => {
         let datePicker = <DateTimePicker
                 value={this.state.date} mode='date'
@@ -68,7 +78,7 @@ export default class AddTask extends Component {
                             <Text style={styles.button}>Cancelar</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={this.save}>
                             <Text style={styles.button}>Salvar</Text>
                         </TouchableOpacity>
                     </View>
