63c52bc4e9cbe8136c64b759690874145fc97ca6
Marlon Almino <alminomarlon@gmail.com>
2023-03-31 10:39:40 -0300

    Modal Adicionar Tarefa #03

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
-import { Text, Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { 
+    TextInput, 
+    Text, 
+    Modal, 
+    View, 
+    StyleSheet,
+    TouchableOpacity,
+    TouchableWithoutFeedback } from 'react-native'
 
 import commonStyles from '../commonStyles'
+
+const initialState = { desc: '' }
 export default class AddTask extends Component {
+
+    state = {
+        ...initialState
+    }
 
     render () {
         return (
@@ -16,6 +29,18 @@ export default class AddTask extends Component {
 
                 <View style={styles.container}>
                     <Text style={styles.header}>Nova Tarefa</Text>
+                    <TextInput style={styles.input}
+                    placeholder='Informe a descrição...'
+                    onChangeText={desc => this.setState({ desc })}
+                    value={this.state.desc} />
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
 
                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
@@ -32,7 +57,6 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0, 0, 0, 0.7)'
     },
     container: {
-        flex: 1,
         backgroundColor: '#FFF'
     },
     header: {
@@ -42,5 +66,23 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         padding: 15,
         fontSize: 18
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        margin: 15,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
     }
 })
