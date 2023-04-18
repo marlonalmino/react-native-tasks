2e7d0a490486536a8ba7e5c5253a5ea231f009ae
Marlon Almino <alminomarlon@gmail.com>
2023-04-18 10:42:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Func 1')
+    res.status(200).send('Meu Backend!')
+})
+
+app.listen(3000, () => {
+    console.log('Backend executando...!')
+})
