7f16c9976c5d470f9e60fdd040dfb21723a55029
Marlon Almino <alminomarlon@gmail.com>
2023-04-18 14:44:52 -0300

    DB Migrations

--- migrations/20230418144447_create_table_tasks.js
+++ migrations/20230418144447_create_table_tasks.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+    return knex.schema.createTable('tasks', (table) => {
+        table.increments('id').primary()
+        table.string('desc').notNullable()
+        table.dateTime('estimateAt')
+        table.dateTime('doneAt')
+        table.integer('userId').references('id').inTable('users').notNullable()
+    })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+    return knex.schema.dropTable('tasks')
+}
