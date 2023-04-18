f8a45b7e6df5f6c085e135a5c792c90fa796ab9e
Marlon Almino <alminomarlon@gmail.com>
2023-04-18 16:09:05 -0300

    API: Cadastro de Usuário (Signup)

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const app = express()
 const db = require('./config/db')
 const consign = require('consign')
 
-consign().then('./config/middlewares.js').into(app)
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.db = db
-
-app.get('/', (req, res) => {
-    console.log('Func 1')
-    res.status(200).send('Meu Backend!')
-})
 
 app.listen(3000, () => {
     console.log('Backend executando...!')
