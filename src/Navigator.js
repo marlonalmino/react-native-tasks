ff8cb229ccda38eb25422a2e6f1916edb6e5b031
Marlon Almino <alminomarlon@gmail.com>
2023-04-25 09:57:36 -0300

    Navegação

---
+++ src/Navigator.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Auth from './screens/Auth'
+import TaskList from './screens/TaskList'
+
+const NativeStack = createNativeStackNavigator()
+
+const AuthNavigator = () => {
+    return (
+        <NativeStack.Navigator screenOptions={{ headerShown: false }}>
+            <NativeStack.Screen name='Auth' component={Auth} />
+            <NativeStack.Screen name='Home' component={TaskList} />
+        </NativeStack.Navigator>
+    )
+}
+
+const Navigator = () => {
+    return (
+        <NavigationContainer>
+            <AuthNavigator />
+        </NavigationContainer>
+    )
+}
+
+export default Navigator
