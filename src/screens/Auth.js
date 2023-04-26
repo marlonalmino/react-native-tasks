4c33f9fc65f6987a05d968d7ac7703f864c9b8d6
Marlon Almino <alminomarlon@gmail.com>
2023-04-25 21:32:43 -0300

    Validando Formulário

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -81,6 +81,17 @@ export default class Auth extends Component {
     }
 
     render() {
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+
+        if (this.state.stageNew) {
+            validations.push(this.state.name && this.state.name.trim().length >= 3)
+            validations.push(this.state.password === this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((total, current) => total && current)
+
         return (
             <ImageBackground source={backgroundImage} style={styles.background}>
                 <Text style={styles.title}>Tasks</Text>
@@ -109,8 +120,9 @@ export default class Auth extends Component {
                             secureTextEntry={true} 
                         />
                     }
-                    <TouchableOpacity onPress={this.signinOrSignup}>
-                        <View style={styles.button}>
+                    <TouchableOpacity onPress={this.signinOrSignup}
+                        disabled={!validForm}>
+                        <View style={[styles.button, validForm ? {} : {backgroundColor: '#AAA'}]}>
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Cadastrar' : 'Entrar'}
                             </Text>
